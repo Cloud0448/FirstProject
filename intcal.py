b99984fdecbe71d1592b74547c7caef3fc1eae35
J_Yoon <cloud00448@gmail.com>
2024-07-01 11:47:44 +0900

    0701 commit

--- intcal.py
+++ intcal.py
@@ -12,3 +12,19 @@ def solution(a, b):
 ##다른 사람 풀이를 보니 이렇게도 된다? 근데 비교하는 두 수 값이 같은 경우는 고려 안 한듯
 def solution(a, b):
     return max(int(str(a) + str(b)), 2 * a * b)
+
+
+# num이 n의 배수이면 1을, 배수가 아니면 0을 return
+def solution(num, n):
+    if num % n == 0:
+        return 1
+    else :
+        return 0
+
+# number가 n과 m의 공배수이면 1, 아니면 0을 return
+def solution(number, n, m):
+    if (number % n == 0) and (number % m ==0) :
+        return 1
+    else:
+        return 0
+
