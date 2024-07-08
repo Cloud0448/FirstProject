adc3fce4ed69c9e02a8b4d5230a8e43f6cce4ee0
J_Yoon <cloud00448@gmail.com>
2024-07-07 17:01:18 -1000

    0708 commit

--- testarray.py
+++ testarray.py
@@ -117,3 +117,20 @@ def solution(arr, delete_list):
     return answer
 
 
+# arr의 앞에서부터 차례대로 원소를 보면서 원소가 a라면 X의 맨 뒤에 a를 a번 추가하는 일을 반복한 뒤의 배열 X를 return
+def solution(arr):
+    answer = []
+    
+    for i in arr:
+        times = 1
+        while times <= i:
+            answer.append(i)
+            times += 1
+
+    print(answer)
+
+## >> 이렇게 하면 더 깔끔하다
+    answer = []
+    for i in arr:
+        answer += [i] * i
+    print(answer)
